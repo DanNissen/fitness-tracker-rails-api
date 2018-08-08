035a10bf59f4aa656c245c32d2ae2d9fc5681361
DanNissen <d.edwardnissen@gmail.com>
2018-08-08 15:15:54 -0400

    Add user ownership to exercises_controller
    -exercises are now open read but protected from update or delete except
    by signed in users who created them

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180806202134) do
+ActiveRecord::Schema.define(version: 20180808190003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20180806202134) do
     t.string "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_exercises_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -56,6 +58,7 @@ ActiveRecord::Schema.define(version: 20180806202134) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "exercises", "users"
   add_foreign_key "workouts", "exercises"
   add_foreign_key "workouts", "users"
 end
